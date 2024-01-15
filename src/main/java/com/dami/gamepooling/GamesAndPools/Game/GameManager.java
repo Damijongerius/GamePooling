64b335179fab831fc113f9cb4d89882b3f5aae8f
damii ii ii <damianojongerius@gmail.com>
2024-01-15 01:40:39 +0100

    basic pooling working

--- src/main/java/com/dami/gamepooling/GamesAndPools/Game/GameManager.java
+++ src/main/java/com/dami/gamepooling/GamesAndPools/Game/GameManager.java
@@ -20,7 +20,7 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-//random chang
+//random chan
 
 public abstract class GameManager extends BukkitRunnable implements Listener {
 
